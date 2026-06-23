f071a73817de9027146270076283186943b32baf
Ricardo J. M. Mosqueda <ric.mmosqueda@gmail.com>
2026-06-23 12:01:57 -0400

    feat: agregando mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ricjdev.github.io',
   base: '/Trabajo-Final-Estadistica/',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [mdx()],
 })
